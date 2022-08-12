a5966387f53ceb67b16cade206353bfe271f30d8
郑宝迪 <zibo.zheng@brantte.com>
2022-08-12 18:08:13 +0800

    增加树到数组的转换

---
+++ src/tree-to-array/transform1.js
@@ -0,0 +1,27 @@
+// 根据分支结点进行平铺
+function transform2(data, adapter) {
+  const {
+    parent_prop,
+    children_prop = 'children',
+    container_prop
+  } = adapter
+
+  const result = []
+
+  function recur(nodes) {
+    for (let i = 0, node, nodeValue, children; i <= nodes.length; i++) {
+      node = nodes[i]
+      nodeValue = { ...(container_prop ? node[container_prop] : node) }
+      children = node[children_prop] || []
+      if (!container_prop) {
+        delete nodeValue[children_prop]
+        parent_prop && delete nodeValue[parent_prop]
+      }
+      result.push(nodeValue)
+      recur(children)
+    }
+  }
+
+  recur(data)
+  return result
+}
---
+++ src/tree-to-array/transform2.js
@@ -0,0 +1,33 @@
+// 根据叶子结点进行平铺
+function transform2(data, adapter) {
+  const {
+    id = 'id',
+    parent_prop = 'parent',
+    children_prop,
+    container_prop
+  } = adapter
+
+  const result = []
+  const collected = new Map()
+
+  function recur(node) {
+    const nodeValue = { ...(container_prop ? node[container_prop] : node) }
+    const nodeId = nodeValue[id]
+    if (collected.has(nodeId)) return
+    const parent = node[parent_prop]
+    if (!container_prop) {
+      delete nodeValue[parent_prop]
+      children_prop && delete nodeValue[children_prop]
+    }
+    collected.set(nodeId, 1)
+    result.push(nodeValue)
+    parent && recur(parent)
+  }
+
+  for (let i = 0; i <= data.length; i++) {
+    recur(data[i])
+  }
+
+  collected.clear()
+  return result
+}

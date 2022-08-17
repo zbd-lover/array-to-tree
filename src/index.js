389abeec55d3b03704620d6248e174653f2ec165
郑宝迪 <zibo.zheng@brantte.com>
2022-08-17 17:55:19 +0800

    入口文件修改

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
-import arrayToTree from './to-tree'
+import a2t from './to-tree'
 import t2a from './to-array'
 
 export const treeToArray = t2a
+const arrayToTree = a2t
+
 export default arrayToTree

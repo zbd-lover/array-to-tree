4c233ce8a89e36bdbe930cd6dc36e516b88629a5
zbd <2243518400@qq.com>
2022-08-17 22:23:29 +0800

    完善package.json

--- index.d.ts
+++ index.d.ts
@@ -1,50 +1,59 @@
-export declare interface AdapterA2T {
-  /** */
+export declare interface OptionA2T {
+  /** Define a property that can differentiate each node, by default 'id'. */
   id?: string  
-  /** */
+  /** Define a property that can differentiate node's parent, by default 'parent_id'. */
   parent_id?: string
-  /** */
+  /** Define a property of node that refers to its parent, by default 'parent' if 'root' as 'leaf'. */
   parent_prop?: string,
-  /** */
+  /** Define a property of node that refers to its children by default 'children' if 'root' as 'branch'. */
   children_prop?: string,
-  /** */
-  container_prop?: string
-  /** */
-  root?: 'branch' | 'leaf'
+  /** Define a property of node that wrap the its value. */
+  container_prop?: string,
+  /**
+   * How to reflect the structure of the tree? choose 'branch' or 'leaf' as 'root' (way to iterate tree).
+   * Determines whether node in result as the topest branch node or leaf node.
+   * By default 'branch'.
+   * */
+  root?: 'branch' | 'leaf',
+  /**
+   * Function will be called more strictly, by default false. It will throw error in follwing cases: <br/>
+   * 1. dulipcated node.id <br/>
+   * 2. unkonwn parent.id <br/>
+   * 3. self.id equals certain ancestor.id
+   *  */
+  strict?: boolean
 }
 
-declare interface OptionA2T extends AdapterA2T {
-  /** */
-  strict?: boolean
-} 
-
 declare interface ArrayToTree {
-  (): void
+  <S = any>(data: any[], option?: OptionA2T): S[]
 }
 
 declare const arrayToTree: ArrayToTree
 
 export default arrayToTree
 
-export declare interface AdapterT2A {
-  /** */
+export declare interface OptionT2A {
+  /**
+   * Define a property that can differentiate each node, by default 'id'.
+   * It's necessary, when 'root' is 'leaf'.
+   * */
   id?: string  
-  /** */
+  /** Does what propery of node refer to its' parent? */
   parent_prop?: string,
-  /** */
+  /** Does what propery of node refer to its' children? */
   children_prop?: string,
-  /** */
+  /** Does what propery of node wrap its' value? */
   container_prop?: string
-  /** */
+  /**
+   * How to reflect the structure of the tree? choose 'branch' or 'leaf' as 'root' (way to iterate tree).
+   * Determines whether node in datasource as the topest branch node or leaf node.
+   * By default 'branch'.
+   * */
   root?: 'branch' | 'leaf'
 }
 
-declare interface OptionT2A extends AdapterT2A {
-  strict?: boolean
-} 
-
 declare interface TreeToArray {
-  (): void
+  <S = any>(data: any, option?: OptionT2A): S[]
 }
 
-declare const treeToArray: TreeToArray
+export declare const treeToArray: TreeToArray
